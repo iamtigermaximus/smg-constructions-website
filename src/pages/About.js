ef3ba65629d8d407576364ad17609720c6207284
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-28 00:09:05 +0200

    make page viewport height 100vh temporarily

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const PageTitle = styled.h1`
+  color: white;
+`
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <Container>
+      <PageTitle>About</PageTitle>
+    </Container>
+  )
 }
 
 export default About
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const PageTitle = styled.h1`
+  color: white;
+`
 
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <Container>
+      <PageTitle>Contact</PageTitle>
+    </Container>
+  )
 }
 
 export default Contact
